18542f7ab667bc3c752230c035b8da4b18d82211
Sean Kelley <seansfkelley@users.noreply.github.com>
2023-10-08 17:54:25 -0400

    Force-add my keyboard configuration.

---
+++ keyboards/ergodox_ez/keymaps/seankelley/config.h
@@ -0,0 +1,15 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#define ORYX_CONFIGURATOR
+#define IGNORE_MOD_TAP_INTERRUPT
+#undef DEBOUNCE
+#define DEBOUNCE 40
+
+#define USB_SUSPEND_WAKEUP_DELAY 0
+#define FIRMWARE_VERSION u8"DARGZ/aE3Ov"
+#define RAW_USAGE_PAGE 0xFF60
+#define RAW_USAGE_ID 0x61
+#define LAYER_STATE_8BIT
+
